544ed43a376e423734e89910133096d9e698f78a
Yaroslav Sazonov <delpass@gmail.com>
2016-07-28 16:22:46 +0300

    fix ice names

--- kurento/IceCandidate.py
+++ kurento/IceCandidate.py
@@ -1,8 +1,8 @@
 
 class IceCandidate(object):
     candidate = None
-    sdp_mid = None
-    sdp_m_line_index = None
+    sdpMid = None
+    sdpMLineIndex = None
 
     def __init__(self, candidate, sdp_mid, sdp_m_line_index):
         """
@@ -13,8 +13,8 @@ class IceCandidate(object):
         """
         super(IceCandidate, self).__init__()
         self.candidate = candidate
-        self.sdp_mid = sdp_mid
-        self.sdp_m_line_index = sdp_m_line_index
+        self.sdpMid = sdp_mid
+        self.sdpMLineIndex = sdp_m_line_index
 
     def get_candidate(self):
         """
@@ -36,7 +36,7 @@ class IceCandidate(object):
         Get sdpMid param
         :return: <string> sdpMid
         """
-        return self.sdp_mid
+        return self.sdpMid
 
     def set_sdp_mid(self, sdp_mid):
         """
@@ -44,14 +44,14 @@ class IceCandidate(object):
         :param sdp_mid:
         :return: <None>
         """
-        self.sdp_mid = sdp_mid
+        self.sdpMid = sdp_mid
 
     def get_sdp_m_line_index(self):
         """
         sdpMLineIndex getter
         :return: <int>
         """
-        return self.sdp_m_line_index
+        return self.sdpMLineIndex
 
     def set_sdp_m_line_index(self, sdp_m_line_index):
         """
@@ -59,4 +59,4 @@ class IceCandidate(object):
         :param sdp_m_line_index: <int>
         :return: <None>
         """
-        self.sdp_m_line_index = sdp_m_line_index
+        self.sdpMLineIndex = sdp_m_line_index
